cb1c8a58c0ba199ced6acd900ef59fd490ca0fa9
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-13 21:45:17 +0200

    add auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import Navbar from '../components/navbar';
-import { MsalProvider } from '@azure/msal-react';
-import { msalInstance } from '../services/msal';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <MsalProvider instance={msalInstance}>
-        <Component {...pageProps} />
-      </MsalProvider>
+      <Component {...pageProps} />
     </>
   );
 }
