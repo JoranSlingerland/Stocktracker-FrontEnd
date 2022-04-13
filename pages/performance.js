5f096350a1eb55bb7f38e17482d7981ce2ac0699
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-13 19:03:18 +0200

    prettier fix(hopefully)

--- pages/performance.js
+++ pages/performance.js
@@ -1,7 +1,7 @@
 const Home = () => (
-    <div className='home'>
-        <h1>What Can I Deploy to Static Apps?</h1>
-    </div>
+  <div className="home">
+    <h1>What Can I Deploy to Static Apps?</h1>
+  </div>
 );
 
 export default Home;
