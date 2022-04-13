37a5b558a70744b2c1e13e5acac0dd91b1c5468e
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-13 22:21:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex=col items-center justify-center w-full h-screen text-center ">
-        <h1 className="text-3xl"> Index{'\n'}</h1>
-        <a href="/.auth/login/aad?post_login_redirect_uri=/">Login</a>
+      <h1 className="text-3xl"> Index{'\n'}</h1>
     </div>
   );
 }
