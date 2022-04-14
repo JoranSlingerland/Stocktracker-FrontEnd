f51017771116e3621f8afcae5a9f0c394a27c5b3
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-14 19:42:24 +0200

    Trying to add a image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Image from 'next/image';
+import mypic from '../public/ms-symbollockup_signin_light.png'
+
 export default function Home() {
   return (
-    <div className="flex flex=col items-center justify-center w-full h-screen text-center ">
-      <h1 className="text-3xl"> Index{'\n'}</h1>
+    
+    <div className="flex flex-col items-center">
+      <Image src={mypic} width={215} height={41}></Image>
+      <div className="py-10 text-4xl"> <h1>Welcome to the stocktracker web app</h1></div>
+      
+      <div className="py-10">02</div>
     </div>
   );
 }
