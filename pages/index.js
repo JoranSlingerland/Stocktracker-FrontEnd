77bf85a1ccdca1b80140c0d2a4c64b0f6e591a5e
joran.slingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-15 14:58:39 +0200

    lint fix

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import mypic from '../public/ms-symbollockup_signin_light.png'
 
 export default function Home() {
   return (
-    
     <div className="flex flex-col items-center">
-      <Image src={mypic} width={215} height={41}></Image>
-      <div className="py-10 text-4xl"> <h1>Welcome to the stocktracker web app</h1></div>
-      
+      <Image src={mypic} width={215} height={41} alt="mslogo"></Image>
+      <div className="py-10 text-4xl">
+        {' '}
+        <h1>Welcome to the stocktracker web app</h1>
+      </div>
+
       <div className="py-10">02</div>
     </div>
   );
