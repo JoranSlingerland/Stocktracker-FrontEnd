71eeb217cebdc00335d6679f3f82bcf52c1217ea
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-17 19:39:00 +0200

    Finally a image : )

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    trailingSlash: true
+  trailingSlash: true,
+  images: {
+    loader: 'custom',
+  },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-import Image from 'next/image';
-import mypic from '../public/ms-symbollockup_signin_light.png';
+import Image from '../components/image'
+import mypic from '../public/images/ms-symbollockup_signin_light.png';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <Image src={mypic} width={215} height={41} alt="mslogo"></Image>
+      
       <div className="py-10 text-4xl">
         {' '}
         <h1>Welcome to the stocktracker web app</h1>
-      </div>
-
-      <div className="py-10">02</div>
+      </div>{' '}
+      <div className="py-10"><Image src={mypic} alt="mypic" /></div>
     </div>
   );
 }
