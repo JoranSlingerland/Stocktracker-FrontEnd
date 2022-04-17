259be6058c97c54958b6738d6232e2b53d833747
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-17 20:03:00 +0200

    rework redirects

---
+++ pages/login.js
@@ -0,0 +1,27 @@
+// pages/login.js
+
+import Link from 'next/link';
+import Image from '../components/image';
+import mypic from '../public/images/ms-symbollockup_signin_light.png';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="py-10 text-4xl">
+        {' '}
+        <h1>Welcome to the stocktracker web app</h1>
+      </div>{' '}
+      <div className="py-10">
+        <Link
+          href={
+            '/.auth/login/aad?post_login_redirect_uri=/authenticated/portfolio/'
+          }
+        >
+          <a>
+            <Image src={mypic} alt="mypic" />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
