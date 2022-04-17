da421e79bebea76b446f770467d86839c197425c
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-17 23:12:34 +0200

    Update settings.js

--- pages/authenticated/settings.js
+++ pages/authenticated/settings.js
@@ -1,8 +1,7 @@
 export default function Home() {
-    return (
-      <div className="flex flex=col items-center justify-center w-full h-screen">
-        <h1 className="text-3xl"> Settings</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex flex=col items-center justify-center w-full h-screen">
+      <h1 className="text-3xl"> Settings</h1>
+    </div>
+  );
+}
