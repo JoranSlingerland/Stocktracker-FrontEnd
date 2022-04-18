d67502558336fe0d18d6541b9f06b56e63c4b0cd
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-18 15:21:06 +0200

    Ant navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,76 +1,59 @@
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
-import { Popover } from '@headlessui/react';
+import { Menu } from 'antd';
+import {
+  AreaChartOutlined,
+  HomeOutlined,
+  InteractionOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import React from 'react';
+import { Link } from 'next/link';
+import UserInfo from './UserInfo';
 
-async function getUserInfo() {
-  try {
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
-  } catch (error) {
-    console.error('No profile could be found');
-    return undefined;
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+  state = {
+    current: undefined,
+  };
+
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
+
+  render() {
+    const { current } = this.state;
+    return (
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="portfolio" icon={<HomeOutlined />}>
+          <a href="/authenticated/portfolio/">Portfolio</a>
+        </Menu.Item>
+        <Menu.Item key="performance" icon={<AreaChartOutlined />}>
+          <a href="/authenticated/performance/">Performance</a>
+        </Menu.Item>
+        <Menu.Item key="actions" icon={<InteractionOutlined />}>
+          <a href="/authenticated/actions/">Actions</a>
+        </Menu.Item>
+        <SubMenu
+          icon={<UserOutlined />}
+          className="ml-auto"
+          key="SubMenu"
+          title={<UserInfo />}
+        >
+          <Menu.Item key="logout">
+            <a href="/.auth/logout?post_logout_redirect_uri=/">logout</a>
+          </Menu.Item>
+          <Menu.Item key="settings">
+            <a href="/authenticated/settings">Settings</a>
+          </Menu.Item>
+        </SubMenu>
+      </Menu>
+    );
   }
 }
 
-function MyPopover() {
-  const [open, setOpen] = useState(false);
-  const [userInfo, setUserInfo] = useState();
-
-  useEffect(() => {
-    (async () => {
-      setUserInfo(await getUserInfo());
-    })();
-  }, []);
-
-  return (
-    <Popover className="">
-      <Popover.Button>
-        <p>{userInfo && userInfo.userDetails}</p>
-      </Popover.Button>
-
-      <Popover.Panel className="absolute z-10">
-        <div className="flex flex-col p-2 break-all bg-gray-100 rounded grow">
-          <Link href="/.auth/logout?post_logout_redirect_uri=/">logout</Link>
-          <Link href="/authenticated/settings">Settings</Link>
-        </div>
-      </Popover.Panel>
-    </Popover>
-  );
-}
-
-export default function header() {
-  const [userInfo, setUserInfo] = useState();
-
-  useEffect(() => {
-    (async () => {
-      setUserInfo(await getUserInfo());
-    })();
-  }, []);
-
-  return (
-    <div>
-      <nav className="flex items-center h-10 px-5 bg-gray-100">
-        {userInfo && (
-          <div className="flex items-center w-3/12 space-x-2">
-            <div>
-              <Link href="/authenticated/portfolio">Portfolio</Link>
-            </div>
-            <div>
-              <Link href="/authenticated/performance">Performance</Link>
-            </div>
-            <div>
-              <Link href="/authenticated/actions">Actions</Link>
-            </div>
-          </div>
-        )}
-        {/* right */}
-        <div className="flex justify-end w-9/12">
-          <MyPopover />
-        </div>
-      </nav>
-      {/* left */}
-    </div>
-  );
-}
+export default App;
