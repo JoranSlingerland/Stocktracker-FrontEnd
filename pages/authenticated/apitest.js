4aefb90fe71add2e8d494c313cba2e365e5257bb
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-18 19:42:13 +0200

    Create apitest.js

---
+++ pages/authenticated/apitest.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+export default function apitest() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [Data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/get_table_data/input_transactions');
+      const data = await response.json();
+      setData(data);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>dashboard</h2>
+        <h2>test - {Data[0].symbol}</h2>
+      </div>
+      <div>
+        <h2>dashboard 2</h2>
+        {Data && Data.length > 0 && Data.map(Data => (
+            <div key={Data.uid}>
+                <h3> {Data.symbol} - {Data.transaction_date} </h3>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+}
