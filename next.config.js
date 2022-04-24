7b9888982c3b8e4eb11c3e6d18109b6d11b3e5cb
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-24 20:31:09 +0200

    Add #18

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   trailingSlash: true,
   images: {
     loader: 'custom',
   },
-};
+});
