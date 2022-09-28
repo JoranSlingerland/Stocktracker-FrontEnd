a5c8ece590ff9e0a91a06409999aa1d30790b01a
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-09-28 15:42:42 +0200

    prettier

--- pages/test/[pid].js
+++ pages/test/[pid].js
@@ -1,10 +1,10 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 const Post = () => {
-  const router = useRouter()
-  const { pid } = router.query
+  const router = useRouter();
+  const { pid } = router.query;
 
-  return <p>Post: {pid}</p>
-}
+  return <p>Post: {pid}</p>;
+};
 
-export default Post
+export default Post;
