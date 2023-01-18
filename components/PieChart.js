6d3b4c7cca83cc55470699eedfc20340f69904ed
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-01-18 01:33:25 +0100

    Add currency formatting

--- components/PieChart.js
+++ components/PieChart.js
@@ -2,8 +2,6 @@ import { Spin } from 'antd';
 import { Chart } from 'primereact/chart';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-
-
 export default function PieChart({ data, isloading }) {
   const formatCurrency = (value) => {
     return value.toLocaleString('nl-NL', {
@@ -24,7 +22,28 @@ export default function PieChart({ data, isloading }) {
   };
   const lightOptions = {
     plugins: {
+      tooltip: {
+        usePointStyle: true,
+        callbacks: {
+          label: function (context) {
+            let index = context.dataIndex;
+            console.log(context);
+            let label = context.label;
+            label += ': ';
+            label += formatCurrency(context.dataset.data[index]);
+            return label;
+          },
+          labelPointStyle: function (context) {
+            console.log(context);
+            return {
+              pointStyle: 'triangle',
+              rotation: 0,
+            };
+          },
+        },
+      },
       legend: {
+        position: 'right',
         labels: {
           color: '#495057',
         },
