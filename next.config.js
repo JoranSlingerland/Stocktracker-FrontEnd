c021f26252a92798000a6f20d9c4052fb1558240
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-01-18 21:48:10 +0100

    Add PWA back #18

--- next.config.js
+++ next.config.js
@@ -4,3 +4,17 @@ module.exports = {
     loader: 'custom',
   },
 };
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  trailingSlash: true,
+  images: {
+    loader: 'custom',
+  },
+});
