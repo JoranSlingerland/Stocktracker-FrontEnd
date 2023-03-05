9ad17bc058469a5730dfc696c045d108bf455c33
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-03-05 16:06:44 +0100

    Moe styling

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,12 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
-      <div className="flex justify-center min-h-screen px-2 xl:px-0">
-        <div className="w-full max-w-7xl">
-          <Component {...pageProps} />
+      <div className="min-h-screen">
+        <Navbar />
+        <div className="flex justify-center px-2 xl:px-0">
+          <div className="w-full max-w-7xl">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </>
